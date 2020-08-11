7376cf75e1987251fa6daab17da853bc391ff967
Tangjiajiang <56879893+Tangjiajiang1@users.noreply.github.com>
2020-08-11 17:46:22 +0800

    Update

--- snell.centos.sh
+++ snell.centos.sh
@@ -15,14 +15,14 @@ if [ -f ${CONF} ]; then
   else
   if [ -z ${PSK} ]; then
     PSK=$(tr -dc A-Za-z0-9 </dev/urandom | head -c 16)
-    echo "Using generated PSK: ${PSK}"
+    echo "Using generated PSK: ${jiajiang1314}"
   else
-    echo "Using predefined PSK: ${PSK}"
+    echo "Using predefined PSK: ${jiajiang1314}"
   fi
   mkdir /etc/snell/
   echo "Generating new config..."
   echo "[snell-server]" >>${CONF}
-  echo "listen = 0.0.0.0:443" >>${CONF}
+  echo "listen = 0.0.0.0:8080" >>${CONF}
   echo "psk = ${PSK}" >>${CONF}
   echo "obfs = http" >>${CONF}
 fi
