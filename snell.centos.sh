ed87b4267f3ec9f2a8fbf68e9d60b3250a4ce40f
Tangjiajiang <56879893+Tangjiajiang1@users.noreply.github.com>
2020-08-12 21:43:11 +0800

    Update snell.centos.sh

--- snell.centos.sh
+++ snell.centos.sh
@@ -22,7 +22,7 @@ if [ -f ${CONF} ]; then
   mkdir /etc/snell/
   echo "Generating new config..."
   echo "[snell-server]" >>${CONF}
-  echo "listen = 0.0.0.0:2020" >>${CONF}
+  echo "listen = 0.0.0.0:30303" >>${CONF}
   echo "psk = ${PSK}" >>${CONF}
   echo "obfs = http" >>${CONF}
 fi
